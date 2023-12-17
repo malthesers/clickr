6ab3b66cd2e24fc77c2f6dcd9912f82ddee485aa
malt9830 <malt9830@stud.kea.dk>
2023-12-17 17:37:02 +0100

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className="min-h-screen grid place-content-center p-24">
+      <div className='text-2xl'>yo</div>
+    </main>
+  )
+}
