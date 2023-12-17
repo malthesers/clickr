db976a5e0e86843a4353a015fa283a4501fb7195
malt9830 <malt9830@stud.kea.dk>
2023-12-17 17:51:24 +0100

    Added initial incrementation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,17 @@
-import Image from 'next/image'
+"use client"
+
+import { useState } from 'react'
 
 export default function Home() {
+  const [clicks, setClicks] = useState<number>(1)
+
+  function incrementClicks() {
+    setClicks(clicks + 1)
+  } 
+
   return (
     <main className="min-h-screen grid place-content-center p-24">
-      <div className='text-2xl'>yo</div>
+      <button onClick={incrementClicks} className='text-5xl'>{ clicks }</button>
     </main>
   )
 }
