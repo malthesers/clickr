065538ccf2bac03b61a2f3a49eeb53703a37cd15
malt9830 <malt9830@stud.kea.dk>
2023-12-17 19:01:36 +0100

    Added current implementation of auto-incrementer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,28 @@
 "use client"
 
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 export default function Home() {
-  const [clicks, setClicks] = useState<number>(1)
+  const [clicks, setClicks] = useState<number>(0)
+  const [clicksPerSecond, setClicksPerSecond] = useState<number>(1)
 
-  function incrementClicks() {
+  function incrementClicks(): void {
     setClicks(clicks + 1)
-  } 
+  }
+
+  const autoIncrementer = () => {
+    setClicks((prevClicks) => prevClicks + 1)
+  }
+
+  useEffect(() => {
+    const incrementerInterval = setInterval(autoIncrementer, 1000)
+
+    return () => clearInterval(incrementerInterval)
+  }, [])
 
   return (
     <main className="min-h-screen grid place-content-center p-24">
-      <button onClick={incrementClicks} className='text-5xl'>{ clicks }</button>
+      <button onClick={incrementClicks} className='text-5xl duration-200 active:scale-95'>{ clicks }</button>
     </main>
   )
 }
