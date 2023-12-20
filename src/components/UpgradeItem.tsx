cefc777bf49e7a3c8f2e636a18b61860192d4248
malt9830 <malt9830@stud.kea.dk>
2023-12-20 18:48:47 +0100

    Fixed cursor pointer overriding not-allowed

--- src/components/UpgradeItem.tsx
+++ src/components/UpgradeItem.tsx
@@ -8,7 +8,7 @@ export default function UpgradeItem({ upgrade }: UpgradeItemProps) {
   const { clicks, buyUpgrade } = useThings()
 
   return (
-    <div onClick={() => buyUpgrade(upgrade)} className={(upgrade.price > clicks ? 'brightness-50 cursor-not-allowed ' : '') + 'rounded-xl border-2 border-slate-300 py-2 px-4 cursor-pointer duration-200 hover:bg-slate-300 hover:text-black'}>
+    <div onClick={() => buyUpgrade(upgrade)} className={(upgrade.price > clicks ? 'brightness-50 cursor-not-allowed' : 'cursor-pointer') + ' rounded-xl border-2 border-slate-300 py-2 px-4 duration-200 hover:bg-slate-300 hover:text-black'}>
       <div className='flex flex-row justify-between'>
         <p className='font-bold'>{ upgrade.name }</p>
         <p>{ upgrade.increase } CpS</p>
