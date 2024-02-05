9fe6d86b79cad07516130cfcfa167754ed395dbd
malt9830 <malt9830@stud.kea.dk>
2024-02-05 19:44:29 +0100

    Fixed typo 'increasesing'

--- src/upgrades/autoClickers.ts
+++ src/upgrades/autoClickers.ts
@@ -8,21 +8,21 @@ export const autoClickersData: AutoClicker[] = [
   },
   {
     name: 'Mega Clicker',
-    description: 'An improved bonus clicker, increasesing clicks per second by 3.',
+    description: 'An improved bonus clicker, increasing clicks per second by 3.',
     increase: 3,
     price: 30,
     owned: 0
   },
   {
     name: 'Giga Clicker',
-    description: 'A gigantic bonus clicker, increasesing clicks per second by 5.',
+    description: 'A gigantic bonus clicker, increasing clicks per second by 5.',
     increase: 5,
     price: 50,
     owned: 0
   },
   {
     name: 'Omega Clicker',
-    description: 'The ultimate bonus clicker, increasesing clicks per second by 10.',
+    description: 'The ultimate bonus clicker, increasing clicks per second by 10.',
     increase: 10,
     price: 100,
     owned: 0
